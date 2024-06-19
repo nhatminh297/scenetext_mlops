7953cda74247d3b9215a1412c677f3e186a5e94d
nhatminh297 <lenhatminh297@gmail.com>
2024-06-19 01:18:11 +0000

    applicaton added

---
+++ config/__init__.py
@@ -0,0 +1,4 @@
+TRACKING_SERVER_URI = "http://localhost:5000"
+MLFLOW_DB_URI = "sqlite:///mlflow.db"
+LOGGED_GLASS_MODEL = 'runs:/a11b9a456a2a42cf859eb143fa40eb2b/glassrunner'
+LOGGED_PADDLE_MODEL = 'runs:/bdb2c42060fe431991b728dbebe71421/paddleocr'
